7287aa0b986bb421d2008c805c327f05db661b7c
Vasil Bit <vasil.bituni@gmail.com>
2021-05-20 16:21:03 +0200

    Add array tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,17 @@
 const calculator = require('./calculator');
 
-test('add 1 plus 2 to equal 3 ', () => {
+xtest('add 1 plus 2 to equal 3 ', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
 
-test('substract 5 minus 3 to equal 2 ', () => {
+xtest('substract 5 minus 3 to equal 2 ', () => {
   expect(calculator.substract(5, 3)).toBe(2);
 });
 
-test('divide 5 with 5 to equal 1 ', () => {
+xtest('divide 5 with 5 to equal 1 ', () => {
   expect(calculator.divide(5, 5)).toBe(1);
 });
 
-test('multiply 5 with 5 to equal 25 ', () => {
+xtest('multiply 5 with 5 to equal 25 ', () => {
   expect(calculator.multiply(5, 5)).toBe(25);
 });
--- ceaser.test.js
+++ ceaser.test.js
@@ -1,17 +1,17 @@
 const ceaser = require('./ceaser');
 
-test('capital and small letters', () => {
+xtest('capital and small letters', () => {
   expect(ceaser('Wow, Zeus!', 4)).toBe('Asa, Diyw!');
 });
 
-test('capital and small letters', () => {
+xtest('capital and small letters', () => {
   expect(ceaser('Wow, Zeus!', 0)).toBe('Wow, Zeus!');
 });
 
-test('only numbers and signs', () => {
+xtest('only numbers and signs', () => {
   expect(ceaser('123%&/,. "§', 4)).toBe('123%&/,. "§');
 });
 
-test('letters are from a to z ', () => {
+xtest('letters are from a to z ', () => {
   expect(ceaser('WwzZzyY', 4)).toMatch(/^[a-zA-Z]*$/);
 });
